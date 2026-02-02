c81b7cb531053ae2a321c11cecc2640aec21e384
NamaraNadaf <namra.nadaf@gmail.com>
2026-02-02 17:05:15 +0530

    Create Script1.sql

---
+++ StoredProcedures/Script1.sql
@@ -0,0 +1,6 @@
+Stored Procedures:
+We write stored procedures
+1. When there is a need to run set of SQL statements together.
+2. Dynamically prepare and execute SQL statements based on the parameters given.
+3. When there is a need to run SQL statements based on some conditions.
+4. When there is a need to run one or more SQL statements in loop.  
